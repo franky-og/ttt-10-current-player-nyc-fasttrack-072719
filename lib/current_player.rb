5d5e37f31ee986dcd3eda61ab3dac7a9cc9ab269
Franky Liu <cuifranky95@gmail.com>
2019-07-28 18:14:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,15 @@
-def turn_count(board)
+def turn_count(board) #function turn_count takes in the array board
   counter = 0
-  board.each do |index|
-    if index == "X" || index == "O"
+  board.each do |index| #itterates through each index of the array board
+    if index == "X" || index == "O" #itterates counter if a player has placed X or O
       counter += 1
     end
   end 
   counter
 end
 
-def current_player(board)
-  if turn_count(board).even?
+def current_player(board) #function current_player takes in the array board
+  if turn_count(board).even? #flow control checking if board is even
     return "X"
   else
     return "O"
